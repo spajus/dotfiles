7663c93314a7030ce53af7835f9af46755226b68
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-11-09 10:48:00 +0200

    Refactor apply.sh

--- apply.sh
+++ apply.sh
@@ -9,26 +9,28 @@ function rsync_dotfiles() {
   rsync --exclude ".git/" \
         --exclude ".DS_Store" \
         --exclude "apply.sh" \
+        --exclude ".vim/" \
         --exclude "README.md" \
         --exclude "osx" \
         --exclude "LICENSE-MIT.txt" \
         -avh --no-perms . ~;
   echo;
 
+  source ~/.bash_profile;
+}
 
+function update_osx() {
   if [[ $OSTYPE == darwin* ]]; then
     echo "Syncing OSX specific stuff";
-    rsync --exclude ".DS_Store" -avh --no-perms osx/ ~;        
+    rsync --exclude ".DS_Store" --exclude ".vim/" -avh --no-perms osx/ ~;        
     plutil -convert binary1 ~/Library/Preferences/com.googlecode.iterm2.plist;
     killall cfprefsd;
     defaults read com.googlecode.iterm2 > /dev/null;
     killall cfprefsd;
     echo;
     echo "Restart iTerm right now to apply settings from configuration";
+    echo;
   fi;
-  echo;
-
-  source ~/.bash_profile;
 }
 
 function update_vim() {
@@ -41,21 +43,24 @@ function update_vim() {
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
   fi;
   echo "Installing vim plugins"
-  vim +PluginInstall +qall
+  vim +PluginInstall! +qall
   echo;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   rsync_dotfiles;
   update_vim;
+  update_osx;
 else
   read -p "Are you sure you want to rsync your .dotfiles to ~? (y/n) " -n 1;
-  echo "";
+  echo;
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     rsync_dotfiles;
     update_vim;
+    update_osx;
   fi;
 fi;
 
 unset rsync_dotfiles;
 unset update_vim;
+unset update_osx;
