72b7ad70934d0ff68c23851b1781f4dcbae3bfd6
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-11-09 08:49:15 +0200

    Add iterm settings

--- apply.sh
+++ apply.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 function rsync_dotfiles() {
+  cd ${0%/*}
   rsync --exclude ".git/" \
         --exclude ".DS_Store" \
         --exclude "apply.sh" \
@@ -8,13 +9,17 @@ function rsync_dotfiles() {
         --exclude "osx" \
         --exclude "LICENSE-MIT.txt" \
         -avh --no-perms . ~;
+  if [[ $OSTYPE == darwin* ]]; then
+    echo "Syncing OSX specific stuff";
+    rsync --exclude ".DS_Store" -avh --no-perms osx/ ~;        
+    plutil -convert binary1 ~/Library/Preferences/com.googlecode.iterm2.plist
+    killall cfprefsd
+    defaults read com.googlecode.iterm2 > /dev/null
+    killall cfprefsd
+    echo "Restart iTerm quickly to apply your profile"
+  fi;
   source ~/.bash_profile;
 }
-
-if [[ $OSTYPE == darwin* ]]; then
-  echo "Syncing OSX specific stuff";
-  rsync --exclude ".DS_Store" -avh --no-perms osx/ ~;        
-fi;
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   rsync_dotfiles;
